df213ad2509d3120605ae99694e5a418525b739c
Lauren <lauren.c4231@gmail.com>
2020-04-17 18:26:19 +0100

    Enable and add primary menu hook

--- themes/practice-theme/functions.php
+++ themes/practice-theme/functions.php
@@ -5,13 +5,14 @@ define('LC_DEV_MODE', true);
 
 // Includes
 include(get_theme_file_path('/includes/front/enqueue.php'));
+include(get_theme_file_path('/includes/setup.php'));
 
 // Hooks
 add_action('wp_enqueue_scripts', 'lc_enqueue');
 add_filter('the_generator', 'remove_wordpress_version');
 add_filter('style_loader_src', 'remove_version_from_style_js');
 add_filter('script_loader_src', 'remove_version_from_style_js');
-
+add_action('after_setup_theme', 'lc_setup_theme');
 function remove_wordpress_version()
 {
     return '';
---
+++ themes/practice-theme/includes/setup.php
@@ -0,0 +1,6 @@
+<?php
+
+function lc_setup_theme()
+{
+    register_nav_menu('primary', __('Primary Menu', 'practice_theme'));
+}
