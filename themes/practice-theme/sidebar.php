f450745ba4be2236a308dddedb5649257f69f245
Lauren <lauren.c4231@gmail.com>
2020-04-17 20:50:16 +0100

    Split out sidebar

---
+++ themes/practice-theme/sidebar.php
@@ -0,0 +1,9 @@
+<div class="sidebar nobottommargin col_last">
+    <div class="sidebar-widgets-wrap">
+        <?php
+        if (is_active_sidebar('lc_sidebar')) {
+            dynamic_sidebar('lc_sidebar');
+        }
+        ?>
+    </div>
+</div>
