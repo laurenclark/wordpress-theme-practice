37d0828cd32f77267cd0c5222303385f1f1db7f8
Lauren <lauren.c4231@gmail.com>
2020-04-18 20:13:51 +0100

    Add theme support for post featured images

--- themes/practice-theme/includes/setup.php
+++ themes/practice-theme/includes/setup.php
@@ -2,14 +2,18 @@
 
 function lc_setup_theme()
 {
+    add_theme_support('post-thumbnails');
     register_nav_menu('primary', __('Primary Menu', 'practice_theme'));
 }
 
+
+// Remove the version from the generator in the head for security
 function remove_wordpress_version()
 {
     return '';
 }
 
+// Strip the version number from styles and scripts
 function remove_version_from_style_js($src)
 {
     if (strpos($src, 'ver=' . get_bloginfo('version'))) {
