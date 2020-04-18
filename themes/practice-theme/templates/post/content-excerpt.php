71e4bebcfd6e9bf03e778d90ab651e1f7034c35a
Lauren <lauren.c4231@gmail.com>
2020-04-18 22:12:02 +0100

    Add dynamic post meta

--- themes/practice-theme/templates/post/content-excerpt.php
+++ themes/practice-theme/templates/post/content-excerpt.php
@@ -14,32 +14,27 @@
         </h2>
     </div>
     <ul class="entry-meta clearfix">
-        <li><i class="icon-calendar3"></i> 10th February 2014</li>
+        <li><i class="icon-calendar3"></i><?php echo get_the_date() ?></li>
         <li>
-            <a href="#">
+            <!-- get_the_author_meta() is not loop-aware so pass in the ID param -->
+            <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')) ?>">
                 <i class="icon-user"></i>
-                admin
+                <?php the_author(); ?>
             </a>
         </li>
         <li>
             <i class="icon-folder-open"></i>
-            <a href="#">General</a>, <a href="#">Media</a>
+            <?php the_category(' '); ?>
         </li>
         <li>
             <a href="#">
                 <i class="icon-comments"></i>
-                13 Comments
+                <?php comments_number('0'); ?>
             </a>
         </li>
     </ul>
     <div class="entry-content">
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate,
-            asperiores quod est tenetur in. Eligendi, deserunt, blanditiis est
-            quisquam doloribus voluptate id aperiam ea ipsum magni aut perspiciatis
-            rem voluptatibus officia eos rerum deleniti quae nihil facilis repellat
-            atque vitae voluptatem libero at eveniet veritatis ab facere.
-        </p>
+        <?php the_excerpt(); ?>
         <a href="<?php the_permalink() ?>" class="more-link">Read More</a>
     </div>
 </div>
