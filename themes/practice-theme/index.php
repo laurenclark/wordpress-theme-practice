61eb95973568fe8c53d1fc5c41acb6baa1c81fea
Lauren <lauren.c4231@gmail.com>
2020-04-18 22:41:41 +0100

    Basic pagination

--- themes/practice-theme/index.php
+++ themes/practice-theme/index.php
@@ -54,12 +54,19 @@ get_header();
                 </div>
                 <div class="row mb-3">
                     <div class="col-12">
-                        <a href="#" class="btn btn-outline-secondary float-left">
+                        <?php
+
+                        next_posts_link('&larr; Older');
+
+                        previous_posts_link('Newer &rarr;');
+
+                        ?>
+                        <!-- <a href="#" class="btn btn-outline-secondary float-left">
                             &larr; Older
                         </a>
                         <a href="#" class="btn btn-outline-dark float-right">
                             Newer &rarr;
-                        </a>
+                        </a>-->
                     </div>
                 </div>
             </div>
