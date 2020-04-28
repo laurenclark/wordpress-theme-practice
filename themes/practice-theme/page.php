022e34b1bce99cf2e2c36a4be9aedcde2dafb963
Lauren <lauren.c4231@gmail.com>
2020-04-28 14:57:35 +0100

    Add subtitle plugin

--- themes/practice-theme/page.php
+++ themes/practice-theme/page.php
@@ -1,87 +1,81 @@
 <?php get_header(); ?>
 
+<?php
+while (have_posts()) {
+    the_post();
+?>
+    <section id="page-title">
+        <div class="container clearfix">
+            <h1><?php the_title(); ?></h1>
+            <span><?php the_subtitle(); ?></span>
+        </div>
+    </section>
+<?php
+}
+// Rewind the loop so it can be run from the start again
+rewind_posts();
+?>
+
+
 <section id="content">
-
     <div class="content-wrap">
-
         <div class="container clearfix">
             <div class="postcontent nobottommargin clearfix">
                 <?php
-                if (have_posts()) {
-                    while (have_posts()) {
-                        the_post();
+                while (have_posts()) {
+                    the_post();
 
-                        global $post;
-                        $author_ID      = $post->post_author;
-                        $author_URL     = get_author_posts_url('ID')
+                    global $post;
+                    $author_ID      = $post->post_author;
+                    $author_URL     = get_author_posts_url('ID')
                 ?>
-                        <div class="single-post nobottommargin">
-                            <div class="entry clearfix">
-
-                                <div class="entry-title">
-                                    <h2>
-                                        <?php the_title(); ?>
-                                    </h2>
-                                </div>
-
-                                <?php
-                                if (has_post_thumbnail()) {
-                                ?>
-                                    <div class="entry-image">
-                                        <a href="<?php the_permalink(); ?>">
-                                            <?php
-                                            the_post_thumbnail('full');
-                                            ?>
-                                        </a>
-                                    </div>
-                                <?php
-                                }
-                                ?>
-
-                                <div class="entry-content notopmargin">
-                                    <?php
-                                    the_content();
-
-                                    $link_pages_config = array(
-                                        'before'    => '<p class="text-center">' . __('Pages:', 'practice_theme'),
-                                        'after'     => '</p>'
-                                    );
-
-                                    wp_link_pages($link_pages_config);
-
-                                    ?>
-
-                                    <div class="tagcloud clearfix bottommargin">
-                                        <?php the_tags('', ' '); ?>
-                                    </div>
-
-
-                                    <div class="clear"></div>
-                                </div>
-                            </div>
-
-                            <div class="line"></div>
-
-
+                    <div class="single-post nobottommargin">
+                        <div class="entry clearfix">
                             <?php
-
-                            if (comments_open() || get_comments_number()) {
-                                comments_template();
+                            if (has_post_thumbnail()) {
+                            ?>
+                                <div class="entry-image">
+                                    <a href="<?php the_permalink(); ?>">
+                                        <?php
+                                        the_post_thumbnail('full');
+                                        ?>
+                                    </a>
+                                </div>
+                            <?php
                             }
-
                             ?>
 
+                            <div class="entry-content notopmargin">
+                                <?php
+                                the_content();
+
+                                $link_pages_config = array(
+                                    'before'    => '<p class="text-center">' . __('Pages:', 'practice_theme'),
+                                    'after'     => '</p>'
+                                );
+
+                                wp_link_pages($link_pages_config);
+
+                                ?>
+
+                                <div class="tagcloud clearfix bottommargin">
+                                    <?php the_tags('', ' '); ?>
+                                </div>
+                                <div class="clear"></div>
+                            </div>
                         </div>
+                        <div class="line"></div>
+                        <?php
+                        if (comments_open() || get_comments_number()) {
+                            comments_template();
+                        }
+                        ?>
+                    </div>
             </div>
-
-    <?php
-                    }
+        <?php
                 }
-
-    ?>
-
-    <?php get_sidebar() ?>
-
+        ?>
+        <?php get_sidebar() ?>
         </div>
     </div>
 </section>
